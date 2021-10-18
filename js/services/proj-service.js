e48e1fd9e4f5e121e50b4c3ca5f4bda1f970a205
Oshra Hartuv <oshrartuv@gmail.com>
2021-10-18 13:21:09 +0300

    added service file

---
+++ js/services/proj-service.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// https://oshrahartuv.github.io/Minesweeper/
+
+
+
+
+var gProjects = [
+    {
+        id: 'mineSweeper',
+        name: 'Minesweeper',
+        title:'Be carefull of the mines!',
+        desc: 'My first sprint',
+        publishedAt: Date.now(),
+        labels: ['Matrix', 'Game']
+    }
+]
